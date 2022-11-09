7a4d3589a43cf542130b89cba0d1ab79d2f7b1da
usgf <2000013081@stu.pku.edu.cn>
2022-11-09 23:38:13 +0800

    login

--- minifun/minifun/views.py
+++ minifun/minifun/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 import json
 from room.models import Room
 from room.admin import id_counter
-
+from room import models
 # this is a test function.
 
 
@@ -21,31 +21,80 @@ def template(request):
 
 
 def login(request):
-    # get param 'username'
-    username = request.GET.get("username")
-    # get param 'password'
-    password = request.GET.get("password")
-    # create a python dictionary
-    resp = {}
-    resp['message'] = "success"
-    resp['succeed'] = True
-    # convert dict to json
-    return HttpResponse(json.dumps(resp))
+    if request.method == "GET":
+
+        # get param 'username'
+        u = request.GET.get("username")
+        # get param 'password'
+        p = request.GET.get("password")
+
+        x = models.UserInfo.objects.filter(username=u)
+        if x:
+            if x.password == p:
+                # create a python dictionary
+                resp = {}
+                resp['message'] = "登陆成功"
+                resp['succeed'] = True
+                # convert dict to json
+                return HttpResponse(json.dumps(resp))
+                return HttpResponse("登录成功")
+            else:
+                resp = {}
+                resp['message'] = "密码错误"
+                resp['succeed'] = False
+                # convert dict to json
+                return HttpResponse(json.dumps(resp))
+                return HttpResponse("密码错误")
+        else:
+            # create a python dictionary
+            resp = {}
+            resp['message'] = "用户名不存在"
+            resp['succeed'] = False
+            # convert dict to json
+            return HttpResponse(json.dumps(resp))
+            return HttpResponse("用户名不存在")
+
 
 # register function
 
 
 def register(request):
-    # get param 'username'
-    username = request.GET.get("username")
-    # get param 'password'
-    password = request.GET.get("password")
-    # create a python dictionary
-    resp = {}
-    resp['message'] = "success"
-    resp['succeed'] = True
-    # convert dict to json
-    return HttpResponse(json.dumps(resp))
+    if request.method == "GET":
+
+        # get param 'username'
+        u = request.GET.get("username")
+        # get param 'password'
+        p = request.GET.get("password")
+
+        # 检查数据库中是否存在该用户名
+        x = models.UserInfo.objects.filter(username=u)
+
+        if x:
+            # 存在
+            
+            
+            resp = {}
+            resp['message'] = "用户名已存在"
+            resp['succeed'] = False
+            # convert dict to json
+            return HttpResponse(json.dumps(resp))
+            return HttpResponse("注册成功")
+
+        else:
+            # 不存在
+            # 定义数据库表userinfo对象
+            user = models.UserInfo()
+            user.username = u
+            user.password = p
+            # 将数据写入数据库
+            user.save()
+            resp = {}
+            resp['message'] = "注册成功"
+            resp['succeed'] = True
+            # convert dict to json
+            return HttpResponse(json.dumps(resp))
+            return HttpResponse("用户名已存在")
+
 
 # create a room function
 
