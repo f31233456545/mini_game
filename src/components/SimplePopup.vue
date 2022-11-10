f6dc118d1eaee23f3d2492c9cbdc826692a8b61f
Fly252 <flyhgx252@gmail.com>
2022-11-10 19:30:50 +0800

    createPopup 注入 $popup 成为全局函数；SimplePopup 组件支持按时关闭；

--- src/components/SimplePopup.vue
+++ src/components/SimplePopup.vue
@@ -1,16 +1,25 @@
 <template>
-    <div class="simplepopup-head" @click="clicked">
-        <span :style="{color: titleColor}">
-            {{title}}: 
+    <div
+        class="simplepopup-head"
+        @mouseover="mouseoverHandler"
+        @mouseout="mouseoutHandler"
+        @click="clickHandler">
+        <span :style="{ color: titleColor }">
+            {{ title }}:
         </span>
-        <span :style="{color: messageColor}">
-            {{message}}
+        <span :style="{ color: messageColor }">
+            {{ message }}
         </span>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            timer: null
+        }
+    },
     props: {
         title: {
             type: String,
@@ -28,12 +37,39 @@ export default {
             type: String,
             default: "#FFFFFF"
         },
+        duration: {
+            type: Number,
+            default: 0
+        },
         remove: Function  // 在 popup.js 中提供，用于 umount 组件
     },
     methods: {
-        clicked() {
+        setTimer(duration) {
+            if (duration > 0) {
+                this.timer = setTimeout(this.remove, duration)
+            }
+        },
+        removeTimer() {
+            if (this.timer !== null) {
+                clearTimeout(this.timer)
+                this.timer = null
+            }
+        },
+        mouseoverHandler() {
+            this.removeTimer(this.duration);
+        },
+        mouseoutHandler() {
+            this.setTimer(this.duration)
+        },
+        clickHandler() {
             this.remove()
         }
+    },
+    mounted() {
+        this.setTimer(this.duration)
+    },
+    unmounted() {
+        this.removeTimer(this.duration)
     }
 }
 </script>
@@ -43,6 +79,5 @@ export default {
     background-color: rgba(0, 0, 0, 0.5);
     border-radius: 4px;
     padding: 3px;
-    float: left;
 }
 </style>
