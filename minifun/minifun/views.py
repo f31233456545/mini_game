c27e0c90f740665e43b553d01ae22315d80bad9a
PC-2022 <2674448306@qq.com>
2022-11-16 15:12:57 +0800

    implement sit and stand

--- minifun/minifun/views.py
+++ minifun/minifun/views.py
@@ -272,13 +272,12 @@ def sit(request):
         return HttpResponse(json.dumps(resp))
     room = Room.object.filter(room_id=my_room_id)[0]
     #player_num has reached to maximum
-    if room.player_num>room:
+    if room.player_num == 8:
         resp['succeed'] = False
         resp['message'] = " seat for player are full. "
         resp['seat_id'] = -1
         return HttpResponse(json.dumps(resp))
     
-
     #modify desk.user_info and assign right seat_id(by calling room.desk.sit)
     #distribute seat for player 
     resp['succeed'] = True
